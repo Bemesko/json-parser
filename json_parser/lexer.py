ea8a931a2d49f503721dfad03e738b35c02591ce
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2023-09-10 10:07:07 -0300

    Rewrite lexing algorithm to use buffers

--- json_parser/lexer.py
+++ json_parser/lexer.py
@@ -11,9 +11,42 @@ def convert_to_token(char: str) -> JsonToken:
             raise JsonLexingError()
 
 
+def tokenize(char: str, buffer: list[JsonToken]) -> tuple[JsonToken, list[JsonToken]]:
+    match char:
+        case "{":
+            return JsonToken.LEFT_CURLY_BRACKET, []
+        case "}":
+            return JsonToken.RIGHT_CURLY_BRACKET, []
+        case '"' if len(buffer) == 0:
+            return JsonToken.OPENING_QUOTE, [JsonToken.OPENING_QUOTE]
+        case '"' if buffer[0] is JsonToken.OPENING_QUOTE and len(buffer) > 1:
+            return JsonToken.STRING, []
+        case char if buffer[0] is JsonToken.OPENING_QUOTE:
+            buffer.append(JsonToken.STRING_CONTENT)
+            return JsonToken.STRING_CONTENT, buffer
+        case _:
+            raise JsonLexingError()
+
+
 def lex_string(string: str) -> list[JsonToken]:
-    tokens = []
-    for char in string:
-        token = convert_to_token(char)
-        tokens.append(token)
+    char_stack = list(string)
+    char_stack.reverse()
+
+    buffer = []
+    tokens: list[JsonToken] = []
+
+    while char_stack:
+        current_char = char_stack.pop()
+
+        current_token, current_buffer = tokenize(current_char, buffer)
+
+        if len(current_buffer) > 0:
+            buffer.extend(current_buffer)
+        else:
+            buffer.clear()
+            tokens.append(current_token)
+
+    if len(buffer) != 0:
+        raise JsonLexingError()
+
     return tokens
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,14 +1,44 @@
 import pytest
-from json_parser.lexer import convert_to_token
+from json_parser.lexer import convert_to_token, lex_string, tokenize
 from json_parser.models import JsonLexingError, JsonToken
 
 
 def test_should_tokenize_left_bracket():
-    assert convert_to_token("{") is JsonToken.LEFT_CURLY_BRACKET
+    token, _ = tokenize("{", [])
+    assert token is JsonToken.LEFT_CURLY_BRACKET
 
 
 def test_should_tokenize_right_bracket():
-    assert convert_to_token("}") is JsonToken.RIGHT_CURLY_BRACKET
+    token, _ = tokenize("}", [])
+    assert token is JsonToken.RIGHT_CURLY_BRACKET
+
+
+def test_should_tokenize_opening_quote():
+    token, _ = tokenize(r'"', [])
+    assert token is JsonToken.OPENING_QUOTE
+
+
+def test_should_tokenize_string_contents():
+    token, buffer = tokenize("a", [JsonToken.OPENING_QUOTE])
+    assert token is JsonToken.STRING_CONTENT
+    assert buffer == [JsonToken.OPENING_QUOTE, JsonToken.STRING_CONTENT]
+
+
+def test_should_tokenize_entire_string():
+    token, buffer = tokenize(r'"', [JsonToken.OPENING_QUOTE, JsonToken.STRING_CONTENT])
+    assert token is JsonToken.STRING
+    assert len(buffer) == 0
+
+
+def test_should_lex_random_string():
+    tokens = lex_string(r'"try to tokenize me"')
+    assert len(tokens) == 1
+    assert tokens[0] is JsonToken.STRING
+
+
+def test_should_not_lex_random_string_without_closing_quotes():
+    with pytest.raises(JsonLexingError):
+        lex_string(r'"try to tokenize me')
 
 
 def test_should_raise_error_if_cannot_tokenize():
